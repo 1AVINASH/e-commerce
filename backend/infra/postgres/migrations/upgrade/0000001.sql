374cc883eee27d252df13c136647277f528ca34f
avinash <avinash24061998@gmail.com>
2025-08-15 13:11:46 +0530

    Adding table for products and users

--- backend/infra/postgres/migrations/upgrade/0000001.sql
+++ backend/infra/postgres/migrations/upgrade/0000001.sql
@@ -1,5 +1,17 @@
 create table users (
     id bigserial,
     name text,
-    email text
+    email text,
+    password text,
+    session_token text
 );
+
+create table products (
+    id bigserial,
+    title text,
+    description text,
+    original_price float,
+    discount float,
+    thumbnail text,
+    photos text
+)
